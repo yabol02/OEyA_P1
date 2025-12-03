d54ebc223013997e76bfdfc408337a9abf1212ba
Yago Boleas <yagobf.ybf@gmail.com>
2025-12-04 00:43:59 +0100

    Refactor Tournament class to enhance ranking sorting and progress reporting; integrate tqdm for match progress visualization and improve play_trace method for detailed match results.

--- limited_sum/tournament.py
+++ limited_sum/tournament.py
@@ -2,6 +2,7 @@ import itertools
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
 from .match import Match
 from .player import Player
@@ -40,7 +41,7 @@ class Tournament:
         # Keys are Player instances and values are their accumulated points.
         self.ranking = {player: 0.0 for player in self.players}  # initial values
 
-    def sort_ranking(self) -> None:
+    def sort_ranking(self, print_ranking: bool = True) -> None:
         """
         Sorts the tournament ranking by score in descending order.
 
@@ -50,31 +51,45 @@ class Tournament:
         :return: None
         :rtype: None
         """
-        sorted_items = sorted(
-            self.ranking.items(), key=lambda item: item[1], reverse=True
+        self.ranking = dict(
+            sorted(self.ranking.items(), key=lambda item: item[1], reverse=True)
         )
 
-        self._sorted_ranking_list = sorted_items
+        if print_ranking:
+            tqdm.write("-" * 30)
+            tqdm.write("FINAL RANKING")
+            tqdm.write("-" * 30)
+            for rank, (player, score) in enumerate(self.ranking.items(), 1):
+                tqdm.write(f"#{rank}. {player.name.ljust(15)} | Score: {score:.2f}")
+            tqdm.write("-" * 30)
+        self.__ranking_sorted = True
 
-        print("-" * 30)
-        print("FINAL RANKING")
-        print("-" * 30)
-        for rank, (player, score) in enumerate(self._sorted_ranking_list, 1):
-            print(f"#{rank}. {player.name.ljust(15)} | Score: {score:.2f}")
-
-    def play(self, print_step=False) -> None:
+    def play(self, print_step=False, ext_progress: bool = False) -> None:
         """
         Simulates the full tournament.
 
         This method simulates all matches among the players using an all-against-all structure.
         It updates the `self.ranking` dictionary with the total points accumulated by each player.
 
+        :param print_step: If True, prints details of each match as it is played.
+        :type print_step: bool
+        :param ext_progress: Optional dictionary to configure an external progress bar.
+        :type ext_progress: dict | None
         :return: None
         :rtype: None
         """
         self.ranking = {player: 0.0 for player in self.players}
 
-        for player_1, player_2 in itertools.combinations(self.players, 2):
+        pairs = list(itertools.combinations(self.players, 2))
+        total_matches = len(pairs)
+
+        for player_1, player_2 in tqdm(
+            iterable=pairs,
+            desc="Tournament progress",
+            total=total_matches,
+            leave=False if ext_progress else True,
+            position=1 if ext_progress else 0,
+        ):
             for r in range(self.repetitions):
                 match = Match(
                     player_1=player_1,
@@ -92,21 +107,28 @@ class Tournament:
                 player_1.clean_history()
                 player_2.clean_history()
 
-        print(f"Tournament finished with {len(self.players)} players and {self.repetitions} repetitions per pair.")
-    
-    def play_trace(self) -> pd.DataFrame:
+    def play_trace(self, ext_progress: bool = False) -> pd.DataFrame:
         """
         Plays the tournament while extracting all the information from each match.
-        
-        
+
+
         :return: A Dataframe with all the games.
         :rtype: pd.DataFrame
         """
-        game_number = 1
         all_match_results = []
+        game_number = 1
         self.ranking = {player: 0.0 for player in self.players}
 
-        for player_1, player_2 in itertools.combinations(self.players, 2):
+        pairs = list(itertools.combinations(self.players, 2))
+        total_matches = len(pairs)
+
+        for player_1, player_2 in tqdm(
+            iterable=pairs,
+            desc="Tournament progress",
+            total=total_matches,
+            leave=False if ext_progress else True,
+            position=1 if ext_progress else 0,
+        ):
             for r in range(self.repetitions):
                 match = Match(
                     player_1=player_1,
@@ -115,10 +137,23 @@ class Tournament:
                     max_rounds=self.max_rounds,
                     error=self.error,
                 )
-                match_result =  match.play_trace()
-                match_result["game_number"] = game_number
-                match_result["repetition"] = r + 1
-                all_match_results.append(match_result)
+
+                match_results = match.play_trace()
+                all_match_results.append(
+                    {
+                        "game_number": game_number,
+                        "repetition": r + 1,
+                        "num_rounds": match_results["rounds"],
+                        "p1_name": match_results["p1_name"],
+                        "p2_name": match_results["p2_name"],
+                        "p1_action": match_results["p1_actions"],
+                        "p2_action": match_results["p2_actions"],
+                        "p1_payoff": match_results["p1_payoffs"],
+                        "p2_payoff": match_results["p2_payoffs"],
+                        "mean_score_p1": match_results["mean_score_p1"],
+                        "mean_score_p2": match_results["mean_score_p2"],
+                    }
+                )
 
                 score_p1, score_p2 = match.score
                 self.ranking[player_1] += score_p1
@@ -128,7 +163,6 @@ class Tournament:
                 player_2.clean_history()
                 game_number += 1
 
-        print(f"Tournament finished with {len(self.players)} players and {self.repetitions} repetitions per pair.")
         return pd.DataFrame(all_match_results)
 
     def plot_results(self) -> None:
@@ -141,11 +175,12 @@ class Tournament:
         :return: None
         :rtype: None
         """
-        if not hasattr(self, "_sorted_ranking_list"):
+        if not hasattr(self, "__ranking_sorted") or not self.__ranking_sorted:
             self.sort_ranking()
 
-        names = [player.name for player, s in self._sorted_ranking_list]
-        scores = [score for p, score in self._sorted_ranking_list]
+        names, scores = map(
+            list, zip(*[(player.name, score) for player, score in self.ranking.items()])
+        )
 
         plt.figure(figsize=(10, 6))
         plt.bar(names, scores, color="skyblue")
@@ -164,3 +199,17 @@ class Tournament:
 
         plt.tight_layout()
         plt.show()
+
+    def __repr__(self) -> str:
+        return (
+            f"Tournament(players_count={len(self.players)!r}, "
+            f"max_rounds={self.max_rounds!r}, stop_prob={self.stop_prob!r}, "
+            f"error={self.error!r}, repetitions={self.repetitions!r})"
+        )
+
+    def __str__(self) -> str:
+        ranking_status = "PENDING"
+        if any(score > 0.0 for score in self.ranking.values()):
+            ranking_status = f"FINAL RANKING"
+
+        return f"All-Against-All Tournament: {len(self.players)} players ({ranking_status})."
